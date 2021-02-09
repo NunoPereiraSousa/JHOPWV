4f81166a24d3325d92bea50fb71e0b9d4c20735a
Nuno Sousa <9180579@esmad.ipp.pt>
2021-02-09 15:15:43 +0000

    Wait client feedback

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,4 @@
+let nav = document.querySelector("nav");
 let hamburger = document.querySelector(".hamburger");
 let nav__items = document.querySelector(".nav__items");
 let lines = document.querySelectorAll(".lines");
@@ -6,14 +7,17 @@ hamburger.addEventListener("click", () => {
   nav__items.classList.toggle("opened");
 
   hamburger.classList.toggle("cross");
-
-  //   lines.forEach(line => {
-  //     line.classList.toggle("cross");
-  //   });
-
-  //   if (nav__items.classList.contains("opened")) {
-  //     nav__items.style.display = "block";
-  //   } else {
-  //     nav__items.style.display = "none";
-  //   }
 });
+
+let previous__scroll__pos = window.pageYOffset;
+let nav__height = `${nav.offsetHeight}`;
+
+window.onscroll = () => {
+  let current__scroll__pos = window.pageYOffset;
+  if (previous__scroll__pos > current__scroll__pos) {
+    nav.style.top = "0";
+  } else {
+    nav.style.top = `-${nav__height}px`;
+  }
+  previous__scroll__pos = current__scroll__pos;
+};
