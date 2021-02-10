cb9e07306aabe8a069b7beb963d52d7a9704710a
Nuno Sousa <9180579@esmad.ipp.pt>
2021-02-10 16:55:56 +0000

    Carousel and adjustments

---
+++ js/carousel.js
@@ -0,0 +1,59 @@
+//
+//   Variables
+//
+//////////////////////////////////////////////////////////////////////
+
+// Play with this value to change the speed
+// let tickerSpeed = 6;
+let isPaused;
+
+let flickity = null;
+const slideshowEl = document.querySelector(".js-slideshow");
+
+//
+//   Functions
+//
+//////////////////////////////////////////////////////////////////////
+
+const update = () => {
+  if (isPaused) return;
+  if (flickity.slides) {
+    // flickity.x = (flickity.x - tickerSpeed) % flickity.slideableWidth;
+    flickity.selectedIndex = flickity.dragEndRestingSelect();
+    flickity.updateSelectedSlide();
+    flickity.settle(flickity.x);
+  }
+  window.requestAnimationFrame(update);
+};
+
+//
+//   Create Flickity
+//
+//////////////////////////////////////////////////////////////////////
+
+flickity = new Flickity(slideshowEl, {
+  autoPlay: false,
+  prevNextButtons: true,
+  pageDots: true,
+  draggable: true,
+  wrapAround: true,
+  selectedAttraction: 0.015,
+  friction: 0.25
+});
+flickity.x = 0;
+
+//
+//   Add Event Listeners
+//
+//////////////////////////////////////////////////////////////////////
+
+flickity.on("dragStart", () => {
+  isPaused = true;
+});
+
+//
+//   Start Ticker
+//
+//////////////////////////////////////////////////////////////////////
+
+update();
---
+++ js/images.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  document.querySelector(".one").style.backgroundImage =
+    "url('../Assets/Help/homeless_outreach.jpg')";
+
+  document.querySelector(".two").style.backgroundImage =
+    "url('../Assets/Help/food_donation.jpg')";
+
+  document.querySelector(".three").style.backgroundImage =
+    "url('../Assets/Help/health_clinic.jpg')";
+};
